c1891506674fcf3d50a3201ebb0e3d8004a67ce0
TheRealWolfman1993 <139507686+TheRealWolfman1993@users.noreply.github.com>
2024-07-03 16:11:04 +0500

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,13 +1,16 @@
 immutable_var = 1, 2, 3, "Четыре", "Пять", False
 print(immutable_var)
-
-# immutable_var[0][0] = 1
-# print(immutable_var)
+print(type(immutable_var))
 
 mutable_list = ["Первый", "Второй", "Третий", "Четвертый", "Пятый"]
+print(type(mutable_list))
 mutable_list[0] = "Десятый"
 mutable_list[1] = "Девятый"
 mutable_list[2] = "Восьмой"
 mutable_list[3] = "Седьмой"
 mutable_list[4] = "Шестой"
 print(mutable_list)
+
+immutable_var[0] = 1
+immutable_var[1] = 1
+immutable_var[2] = 1
