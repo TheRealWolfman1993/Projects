2008c47843f01832b8690b08b583baf38255f214
TheRealWolfman1993 <139507686+TheRealWolfman1993@users.noreply.github.com>
2024-07-07 16:41:04 +0500

    Add files via upload

---
+++ Module #1/4peremennih.py
@@ -0,0 +1,8 @@
+course = "Python" #Название курса
+TotalTasks = 12 #Количество выполненных ДЗ
+NumberOfHours = 1.5 #Количество затраченных часов
+Time_for_one_task = NumberOfHours / TotalTasks #Среднее время выполнения
+print("Курс:", course)
+print("Всего задач:", TotalTasks)
+print("Затрачено часов:", NumberOfHours)
+print("Среднее время выполнения:", Time_for_one_task, "часа(-ов).")
---
+++ Module #1/DynamicTyping.py
@@ -0,0 +1,9 @@
+name = "Захар" #Имя
+print("Имя:", name)
+age = int(31) #Возраст
+print("Age:", age)
+new = 1
+age = age + new #Новый возраст
+print("New Age:", age)
+is_student = True
+print("Is Student:", is_student)
