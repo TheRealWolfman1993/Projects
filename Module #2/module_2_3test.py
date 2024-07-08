9f1c57a9eb01e8f17b495fd010624e8e06691265
TheRealWolfman1993 <139507686+TheRealWolfman1993@users.noreply.github.com>
2024-07-08 23:32:02 +0500

    Add files via upload

---
+++ Module #2/module_2_3test.py
@@ -0,0 +1,50 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while my_list[0] >= 0:
+    if my_list[0] > 0:
+        print(my_list[0])
+        break
+while my_list[1] >= 0:
+    if my_list[1] > 0:
+        print(my_list[1])
+        break
+while my_list[2] >= 0:
+    if my_list[2] > 0:
+        print(my_list[2])
+        break
+while my_list[3] >= 0:
+    if my_list[3] > 0:
+        print(my_list[3])
+        break
+while my_list[4] >= 0:
+    if my_list[4] > 0:
+        print(my_list[4])
+        break
+while my_list[5] >= 0:
+    if my_list[5] > 0:
+        print(my_list[5])
+        break
+while my_list[6] >= 0:
+    if my_list[6] > 0:
+        print(my_list[6])
+        break
+while my_list[7] >= 0:
+    if my_list[7] > 0:
+        print(my_list[7])
+        break
+while my_list[8] >= 0:
+    if my_list[8] > 0:
+        print(my_list[8])
+        break
+while my_list[9] >= 0:
+    if my_list[9] > 0:
+        print(my_list[9])
+        break
+while my_list[10] >= 0:
+    if my_list[10] > 0:
+        print(my_list[10])
+        break
+while my_list[11] >= 0:
+    if my_list[11] > 0:
+        print(my_list[11])
+        break
