0d96f17995eb9d57fd97704418aa54911c842fc3
TheRealWolfman1993 <139507686+TheRealWolfman1993@users.noreply.github.com>
2024-07-11 21:02:42 +0500

    Update module_2_4(3).py

--- Module #2/module_2_4(3).py
+++ Module #2/module_2_4(3).py
@@ -1,25 +1,25 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-prime = []
-not_prime = []
-prime_not_prime_dict = {}
-is_prime = True
+primes = []
+not_primes = []
+primes_not_primes_dict = {}
+is_primes = True
 
 for i in numbers:
     if i != 1:
-        prime_not_prime_dict.update({i: is_prime})
+        primes_not_primes_dict.update({i: is_primes})
 
-for i in list(prime_not_prime_dict.keys()):
+for i in list(primes_not_primes_dict.keys()):
     for j in range(2, i):
         if i % j == 0:
-            prime_not_prime_dict.update({i: False})
+            primes_not_primes_dict.update({i: False})
             break
 
-for i in list(prime_not_prime_dict.keys()):
-    if prime_not_prime_dict[i] == is_prime:
-        prime.append(i)
+for i in list(primes_not_primes_dict.keys()):
+    if primes_not_primes_dict[i] == is_primes:
+        primes.append(i)
     else:
-        not_prime.append(i)
+        not_primes.append(i)
 
-print('Prime: ', prime)
-print('Non prime: ', not_prime)
+print('Primes: ', primes)
+print('Not Primes: ', not_primes)
