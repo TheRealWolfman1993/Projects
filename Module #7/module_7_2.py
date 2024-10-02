9b1e9b22e3e4c2bc540632b3c9c5cba2cb691b5b
TheRealWolfman1993 <139507686+TheRealWolfman1993@users.noreply.github.com>
2024-10-02 22:13:22 +0500

    Update module_7_2.py

--- Module #7/module_7_2.py
+++ Module #7/module_7_2.py
@@ -1,47 +1,20 @@
-class Product:
-    def __init__(self, name, weight, category):
-        self.name = name
-        self.weight = weight
-        self.category = category
+def custom_write(file_name, strings):
+    strings_positions = {}
+    file = open(file_name, 'w', encoding='utf-8')
 
-    def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}'
+    for i in range(len(strings)):
+        strings_positions[i+1, file.tell()] = strings[i]
+        file.write(f'{strings[i]}\n')
+    file.close()
+    return strings_positions
 
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
 
-class Shop:
-    def __init__(self, __file_name='products.txt'):
-        self.__file_name = __file_name
-
-    def get_products(self):
-        file = open(self.__file_name, 'r')
-        text_file = file.read()
-        file.close()
-        return text_file
-
-    def add(self, *products):
-        text_file = self.get_products()
-        file = open(self.__file_name, 'a')
-
-        for product in products:
-            if product.name not in text_file:
-                file.write(f'{product}\n')
-            else:
-                print(f'Продукт {product.name} уже есть в магазине')
-        file.close()
-
-
-def main():
-    s1 = Shop()
-    p1 = Product('Potato', 50.5, 'Vegetables')
-    p2 = Product('Spaghetti', 3.4, 'Groceries')
-    p3 = Product('Potato', 5.5, 'Vegetables')
-
-    print(p2)  # __str__
-
-    s1.add(p1, p2, p3)
-
-    print(s1.get_products())
-
-
-if __name__ == '__main__':
-    main()
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
