07d2a458fcca39767b291c7111a0cfc518b14367
TheRealWolfman1993 <139507686+TheRealWolfman1993@users.noreply.github.com>
2024-11-25 03:01:38 +0500

    Add files via upload

---
+++ Module #9/module_9_6.py
@@ -0,0 +1,11 @@
+from itertools import product
+
+def all_variants(text):
+    for i in range(1, len(text) + 1):
+        for ch in product(text, repeat=i):
+            if "".join(ch) in text:
+                yield "".join(ch)
+
+a = all_variants("abc")
+for i in a:
+    print(i)
